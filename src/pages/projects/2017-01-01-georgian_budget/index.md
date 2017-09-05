151e2324dc8c0a2378cb5b2719a7461083ec7d14
nwshane <django09@gmail.com>
2017-09-05 09:25:27 -0400

    Displaying more frontmatter data on project show page.

--- src/pages/projects/2017-01-01-georgian_budget/index.md
+++ src/pages/projects/2017-01-01-georgian_budget/index.md
@@ -2,14 +2,12 @@
 path: '/projects/georgian_budget'
 title: 'Georgian Budget'
 date: "2017-01-01 14:00:00"
+tags: "open-source website jumpstart-georgia ruby rails javascript react"
+mainUrl: "https://biujeti.jumpstart.ge"
+githubUrl: "https://github.com/JumpStartGeorgia/Georgian-Budget-Public"
 ---
 
-tags: "open-source website jumpstart-georgia ruby rails javascript react"
-github-url: "https://github.com/JumpStartGeorgia/Georgian-Budget-Public"
-main-url: "https://biujeti.jumpstart.ge"
-main-image-path: "/assets/images/Georgian-Budget-National-Budget.png"
-published: true
-
+main-image-path: "./Georgian-National-Budget.png"
 
 JumpStart Georgia requested data about the national Georgian budget from the Georgian government in 2016. We received spreadsheets and PDFs containing valuable, but disconnected, financial data.
 
--- src/templates/project.js
+++ src/templates/project.js
@@ -3,8 +3,28 @@ import Layout from '~/src/components/Layout'
 
 const ProjectTemplate = ({data}) => {
   const project = data.markdownRemark
+  const {frontmatter} = project
   return (
     <Layout>
+      <h1>
+        {frontmatter.title}
+      </h1>
+      {frontmatter.mainUrl && (
+        <p>
+          <a target='_blank' href={frontmatter.mainUrl}>
+            {frontmatter.mainUrl}
+          </a>
+        </p>
+      )}
+      {frontmatter.githubUrl && (
+        <p>
+          <a target='_blank' href={frontmatter.githubUrl}>
+            {frontmatter.githubUrl}
+          </a>
+        </p>
+      )}
+      <p>Date: {frontmatter.date}</p>
+      <p>Tags: {frontmatter.tags}</p>
       <div dangerouslySetInnerHTML={{ __html: project.html }} />
     </Layout>
   )
@@ -18,8 +38,11 @@ export const pageQuery = graphql`
       html
       frontmatter {
         date
+        tags
         path
         title
+        mainUrl
+        githubUrl
       }
     }
   }
