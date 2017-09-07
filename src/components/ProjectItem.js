8b47c40b17f35baa46b026eedb7f4be9c8801574
nwshane <django09@gmail.com>
2017-09-07 16:27:25 -0400

    Styled project titles on project index as Patua One.

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import SharpImage from '~/src/components/SharpImage'
+import styled from 'styled-components'
+
+const H2 = styled.h2`
+  font-family: "Patua One",sans-serif;
+`
 
 const ProjectItem = ({node: {frontmatter}}) => (
   <li>
     <Link to={`/projects/${frontmatter.slug}`}>
-      <h2>{frontmatter.title}</h2>
+      <H2>{frontmatter.title}</H2>
       {frontmatter.featuredImage && (
         <SharpImage imageData={frontmatter.featuredImage} alt='Featured Project Image' />
       )}
