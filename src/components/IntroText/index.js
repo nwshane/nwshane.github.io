0562e64f5568c6b516616d7535eadd63de18aabe
nwshane <django09@gmail.com>
2017-09-08 11:19:26 -0400

    Made IntroText component for home page.

---
+++ src/components/IntroText/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Greeting = styled.p`
+  font-size: calc(2.6rem + 4vw);
+  font-family: "Patua One",sans-serif;
+  margin-top: 1.9em;
+  margin-bottom: 1.1em;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const IntroP = styled.p`
+  font-size: calc(1.2rem + 2vw);
+  line-height: 1.35em;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const IntroText = () => (
+  <main>
+    <Greeting>
+      Hi, I'm Nathan.
+    </Greeting>
+    <IntroP>
+      I like to build things on the world wide web. I also enjoy cooking, singing, and meditating.
+    </IntroP>
+    <IntroP>
+      Check out my <Link to='/projects'>work</Link> and my <a target='_blank' href='https://github.com/nwshane'>Github profile</a>, and don't hesitate to <a target='_blank' href='mailto:nathanwshane@gmail.com'>get in touch</a>.
+    </IntroP>
+  </main>
+)
+
+export default IntroText
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,13 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import Layout from '~/src/components/Layout'
 import HelmetTitle from '~/src/components/HelmetTitle'
 import gatsbyConfig from '~/gatsby-config'
-import styled from 'styled-components'
-
-const Greeting = styled.p`
-  font-size: calc(2.6rem + 4vw);
-  font-family: "Patua One",sans-serif;
-  margin-top: 1.9em;
-  margin-bottom: 1.1em;
-  margin-left: auto;
-  margin-right: auto;
-`
-
-const IntroP = styled.p`
-  font-size: calc(1.2rem + 2vw);
-  line-height: 1.35em;
-  margin-left: auto;
-  margin-right: auto;
-`
+import IntroText from '~/src/components/IntroText'
 
 const HomePage = () => (
   <Layout>
     <HelmetTitle title={gatsbyConfig.siteMetadata.title} />
-    <main>
-      <Greeting>
-        Hi, I'm Nathan.
-      </Greeting>
-      <IntroP>
-        I like to build things on the world wide web. I also enjoy cooking, singing, and meditating.
-      </IntroP>
-      <IntroP>
-        Check out my <Link to='/projects'>work</Link> and my <a target='_blank' href='https://github.com/nwshane'>Github profile</a>, and don't hesitate to <a target='_blank' href='mailto:nathanwshane@gmail.com'>get in touch</a>.
-        </IntroP>
-    </main>
+    <IntroText />
   </Layout>
 )
 
