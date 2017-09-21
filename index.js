ff19c8c6f7d53c9e6b83f60927cfe09150645cd6
nwshane <django09@gmail.com>
2017-09-20 22:17:12 -0400

    Removed unneeded index.js and related dependency on express.

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'))
-
-const getPort = () => process.env.PORT || 8000
-console.log(`Running static app at port ${getPort()}`)
-app.listen(getPort())
