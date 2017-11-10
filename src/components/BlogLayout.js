259be31a6be557f2a0d3b1a5061b56ef7a9d3c4e
nwshane <django09@gmail.com>
2017-11-10 17:29:04 -0500

    Increased specificy of BlogLayout style so that its max-width overrides Layout style.

--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -1,8 +1,13 @@
 import Layout from '~/src/components/Layout'
 import styled from 'styled-components'
 
+// Style is written with && {} so that the class is output as .class.class,
+// causing it to override Layout's style
+// Source: https://github.com/styled-components/styled-components/issues/85#issuecomment-253965711
 const BlogLayout = styled(Layout)`
-  max-width: 75rem;
+  && {
+    max-width: 75rem;
+  }
 `
 
 export default BlogLayout
